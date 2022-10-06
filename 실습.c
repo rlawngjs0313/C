f7c343a7be54b06806ccdd261051e7dd71194562
JuHeon <rlawngjs0313@naver.com>
2022-10-06 17:01:30 +0900

    실습

--- 실습.c
+++ 실습.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-    int 변수 = 0, j = 1;
-    printf("%d %d", 변수, j);
+    int L1[10] = {0};
+    for(int _ = 0; _ < 10; _++){
+        scanf("%d", &L1[_]);
+        getchar();
+    }
+    for(int _ = 0; _ < 10; _++){
+        printf("%d", L1[_]);
+    }
 }
