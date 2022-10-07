bdd3bccd78ab5a3d4a45bc9b164d0f36f400dde3
JuHeon <rlawngjs0313@naver.com>
2022-10-07 23:32:49 +0900

    .

--- 실습.c
+++ 실습.c
@@ -2,12 +2,6 @@
 
 int main()
 {
-    int L1[10] = {0};
-    for(int _ = 0; _ < 10; _++){
-        scanf("%d", &L1[_]);
-        getchar();
-    }
-    for(int _ = 0; _ < 10; _++){
-        printf("%d", L1[_]);
-    }
+    int number = 2; //홀수, 짝수 판별할 변수 number 선언
+    printf(number%2 ? "정수 %d는 홀수입니다.":"정수 %d는 짝수입니다.", number, number); // number가 2로 나누었을때 0이라면 짝수, 그 이상이라면 홀수임을 표현
 }
