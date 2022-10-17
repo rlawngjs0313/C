006deb2d8d56031cd5b04031bcc2744a0f7473b6
JuHeon <rlawngjs0313@naver.com>
2022-10-17 12:18:01 +0900

    ....

--- 과제1_202210904.c
+++ 과제1_202210904.c
@@ -1,11 +1,1 @@
 #include <stdio.h>
-int main()
-{
-    int data = 0;
-    printf("학번을 입력하세요: ");
-    scanf("%d", &data);
-    printf("%d\n", data);
-    printf("%x\n", data);
-    printf("%lf\n", (double)data);
-    printf("%c\n", (char)data);
-}
