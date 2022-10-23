85af9bc94a51355c1a7ada8ec3ffbdd17abf0ca2
JuHeon <rlawngjs0313@naver.com>
2022-10-23 20:52:07 +0900

    C언어 컴파일러 세팅 & 간단한 예제 풀이

--- 과제1_202210904.c
+++ 과제1_202210904.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
-int main(){
-    
+int main()
+{
+    int N = 0;
+    char result[100] = "";
+    int pos = 0;
+    scanf("%d", &N);
+
+    while (pos > -1)
+    {
+        if (N == 0 || N == 1)
+        {
+            result[pos++] = N;
+            break;
+        }
+        else
+        {
+            result[pos++] = N % 2;
+            N /= 2;
+        }
+    }
+
+    for (int i = 0; i < pos; i++)
+    {
+        printf("%c", result[i]);
+    }
 }
--- 실습.c
+++ 실습.c
@@ -1,28 +1,10 @@
 #include <stdio.h>
 
-int main()
-{
-    typedef int 정수;
-    typedef double 실수;
-    실수 Data[5][4] = {{0}, {0}, {0}, {0}, {0}};
-    for (정수 i = 0; i < 5; i++)
-    {
-        정수 국어점수 = 0, 영어점수 = 0, 수학점수 = 0;
-        printf("학생%d의 국어, 영어, 수학점수를 입력하세요. ex)80 70 80: ", i + 1);
-        scanf("%d %d %d", &국어점수, &영어점수, &수학점수);
-        실수 평균 = (국어점수 + 영어점수 + 수학점수) / 3.0;
-        실수 Temp[4] = {평균, 국어점수, 영어점수, 수학점수};
-        for (정수 j = 0; j < 4; j++)
-        {
-            Data[i][j] = Temp[j];
-        }
-    }
-    for (정수 i = 0; i < 5; i++)
-    {
-        printf("___________________________________ : ");
-        printf("학생%d의 평균: %.2lf \n", i + 1, Data[i][0]);
-        printf("학생%d의 국어점수: %.0f \n", i + 1, Data[i][1]);
-        printf("학생%d의 영어점수: %.0f \n", i + 1, Data[i][2]);
-        printf("학생%d의 수학점수: %.0f \n", i + 1, Data[i][3]);
+int main(){
+    int sum = 0x40302010;
+    unsigned char* p = (unsigned char*)&sum;
+    printf("Sum is %x\n", sum);
+    for (int i = 0; i < 4; i++){
+        printf("%p\t%x\n", p+i, p[i]);
     }
 }
