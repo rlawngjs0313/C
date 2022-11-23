26c184804035b8ad171366adab377336cd83f3ac
JuHeon <rlawngjs0313@naver.com>
2022-11-23 14:34:04 +0900

    setting

--- t.cpp
+++ t.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
+#include <deque>
+using namespace std;
 
 int main(){
-    
+    int t;
+    cin >> t;
+    for (int _ = 0; _ < t; _++){
+        int n;
+        cin >> n;
+        deque<int> L1(n);
+
+        for (int i = 0; i < n; i++){
+            cin >> L1[i];
+        }
+
+        deque<int>::iterator iter;
+        for (iter = L1.begin(); iter != L1.end(); iter++){
+            deque<int> temp(L1);
+            temp.erase(iter);
+            cout << *iter - *max_element(temp.begin(), temp.end()) << " ";
+        }
+    }
 }
